7b58a148613efdc15c326900b9b358c47dfb9af9
Jason Abbott <dev@trailimage.com>
2019-07-27 17:35:44 -0600

    add publish task to stencil template

--- generators/stencil/templates/-stencil.config.ts
+++ generators/stencil/templates/-stencil.config.ts
@@ -1,30 +1,30 @@
-import { Config } from "@stencil/core";
-import { sass } from "@stencil/sass";
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: "toba",
-  outputTargets: [
-    { type: "dist" },
-    //{ type: 'docs' },
-    {
-      type: "www",
-      serviceWorker: null // disable service workers
-    }
-  ],
-  plugins: [
-    sass({
-      injectGlobalPaths: [
-        // paths are relative to project root
-        "./node_modules/@toba/style/dist/toba/sass/mixins.scss"
-      ]
-    })
-  ],
-  copy: [
-    {
-      src: "**/**/i18n/*.json",
-      dest: "assets/i18n",
-      warn: true
-    }
-  ],
-  globalStyle: "node_modules/@toba/style/dist/toba/toba.css"
+   namespace: '<%= name %>',
+   outputTargets: [
+      { type: 'dist' },
+      //{ type: 'docs' },
+      {
+         type: 'www',
+         serviceWorker: null // disable service workers
+      }
+   ],
+   plugins: [
+      sass({
+         injectGlobalPaths: [
+            // paths are relative to project root
+            './node_modules/@toba/style/dist/toba/sass/mixins.scss'
+         ]
+      })
+   ],
+   copy: [
+      {
+         src: '**/**/i18n/*.json',
+         dest: 'assets/i18n',
+         warn: true
+      }
+   ],
+   globalStyle: 'node_modules/@toba/style/dist/toba/toba.css'
 };
